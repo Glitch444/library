58fb740196e32631ee00e59f308596e5e0f48c38
Abaz <Abaz95@live.co.uk>
2024-10-28 16:06:45 +0000

    testing

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const parentContainer = document.querySelector(".parent-container")
 const newBookBtn = document.getElementById("new-book-btn");
 const newBookForm = document.getElementById("new-book-form");
-
-const AddNewBookBtn = document.getElementById("add-new-book-btn");
+const AddNewBookBtn = document.getElementById("add-btn");
 const newBookTitle = document.getElementById("title");
 const newBookAuthor = document.getElementById("author"); 
 
-
+// toggle visibility
 newBookBtn.addEventListener("click", showHide);
 function showHide() {
-    if (newBookForm.style.visibility === "hidden"){
+    const currentVisibility = window.getComputedStyle(newBookForm).visibility;
+    if (currentVisibility === "hidden") {
         newBookForm.style.visibility = "visible";
     }
     else {
@@ -17,8 +18,12 @@ function showHide() {
 };
 
 
+
+// retrieve saved data 
 let myLibrary = JSON.parse(localStorage.getItem("myLibrary")) || [];
 
+
+// object
 function Book(name, author) {
     this.name = name;
     this.author = author;
@@ -49,28 +54,71 @@ myLibrary.forEach(displayBook);
     
 function displayBook(book) {
     const newDiv = document.createElement("div");
-    const newBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
     const newPara = document.createElement("p");
 
     newDiv.classList.add("displayed-item");
 
     newDiv.setAttribute("data-id", book.id);
     
-    newBtn.classList.add("displayed-item-btn");
+    newDiv.appendChild(removeBtn);
+    newDiv.appendChild(editBtn);
+    newDiv.appendChild(newPara);
+    parentContainer.appendChild(newDiv);
 
-    newBtn.addEventListener("click", function () {
+    newPara.textContent = `${book.name}, ${book.author}`;
+    removeBtn.textContent = " X ";
+    editBtn.textContent = "edit";
+
+
+    removeBtn.addEventListener("click", function() {
         removeBook(newDiv, book.id);
     });
 
-    newDiv.appendChild(newBtn);
-    newDiv.appendChild(newPara);
-    document.body.appendChild(newDiv);
-
-    newPara.textContent = `${book.name}, ${book.author}`;
-    newBtn.textContent = " X ";
-
+    editBtn.addEventListener("click", function() {
+        editBook(newDiv, newPara, book);
+    });
 }
 
+function editBook(newDiv, newPara, book) {
+ 
+    const newInput = document.createElement("input");
+    newInput.value = newPara.textContent;
+    newDiv.appendChild(newInput);
+    
+    const paraCheck = newDiv.querySelector("p");
+    if (paraCheck) {
+        paraCheck.remove();
+    }
+
+    newInput.focus();
+
+
+    newInput.addEventListener("blur", function() {
+        const inputText = newInput.value;
+        const [newTitle, newAuthor] = inputText.split(",").map(item => item.trim());
+
+        book.name = newTitle;
+        book.author = newAuthor;
+
+        localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+
+        const updatedPara = document.createElement("p");
+        updatedPara.textContent = `${newTitle}, ${newAuthor}`;
+        newDiv.appendChild(updatedPara);
+
+        newInput.remove();
+
+           
+    });
+
+    newInput.addEventListener("keyup", function(event) {
+        if(event.key === "Enter") {
+            newInput.blur();
+        }
+    })
+}
 
 function removeBook(newDiv, bookId){
     newDiv.remove();
